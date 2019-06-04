efa1b8ab787dae175c352a5764aa5d5ebc35e86e
Matvey <ratu1800@yandex.ru>
2019-06-05 00:20:47 +0300

    Закончил, осталось доделать кнопку "показать еще 20"

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-import requests,lxml
+import requests,lxml,re,csv,itertools
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -52,41 +52,151 @@ def get_html_company(href_company):
                   'cooks':content_dict[23],
                   'place_for_walk':content_dict[24],
                   'other':content_dict[25]}
+    return data_content
+def href_human(content_html):
     href_human_list=[]
-    for j in dict.values(data_content):
+    for j in dict.values(content_html):
         soup=BeautifulSoup(j,'lxml')
         div_human=soup.find_all('div',class_='userCard')
         for k in div_human:
             href_human=k.find_all('a',class_='userCard_name')
             for v in href_human:
                 href_human_list.append(v.get('href'))
-    for n in dict.values(data_content):
+    return href_human_list
+def href_human_pro(content_html):
+    href_human_list_pro=[]
+
+    for n in dict.values(content_html):
         soup=BeautifulSoup(n,'lxml')
         div_human=soup.find_all('div',class_='userCard-pro')
         for s in div_human:
-            href_human=s.find_all('a',class_='userCard_name')
+            href_human_pro=s.find_all('a',class_='userCard_name')
+            for l in href_human_pro:
+                href_human_list_pro.append(l.get('href'))
+
+
+    return href_human_list_pro
+def get_html_human_pro(human_pro):
+    get_html_human_pro_list=[]
+    for i in human_pro:
+        r=requests.get(i)
+        get_html_human_pro_list.append(r.text)
+    return get_html_human_pro_list
+def get_html_human(human):
+    get_html_human=[]
+    for i in human:
+        r=requests.get(i)
+        get_html_human.append(r.text)
+    return get_html_human
+
+
+def get_phone_number_pro(html_human_pro):
+    nomer_pro=[]
+    for i in html_human_pro:
+        soup=BeautifulSoup(i,'lxml')
+        phone=soup.find_all('div',class_='content_l')
+        for j in phone:
+            nomer_pro.append(j.find('div',id='meta_info').find('div',text=re.compile('\+79')).text)
+    return nomer_pro
+def get_phone_nomer(html_human):
+    nomer = []
+    for i in html_human:
+        soup = BeautifulSoup(i, 'lxml')
+        phone = soup.find_all('div', class_='content_l')
+        try:
+            for j in phone:
+                nomer.append(j.find('div', id='meta_info').find('div', text=re.compile('\+79')).text)
+        except:
+            continue
+
+
+    return nomer
+def get_name_company_pro(content_html):
+    href_human_list_pro = []
+
+    for n in dict.values(content_html):
+        soup = BeautifulSoup(n, 'lxml')
+        div_human = soup.find_all('div', class_='userCard-pro')
+        for s in div_human:
+            href_human = s.find_all('a', class_='userCard_name')
             for l in href_human:
-                href_human_list.append(l.get('href'))
-    href_human_list = sorted(href_human_list)
+                href_human_list_pro.append(l.get('href').split('/')[3])
+
+    return href_human_list_pro
+def get_name_company(content_html):
+    href_human_list = []
+    for j in dict.values(content_html):
+        soup = BeautifulSoup(j, 'lxml')
+        div_human = soup.find_all('div', class_='userCard')
+        for k in div_human:
+            href_human = k.find_all('a', class_='userCard_name')
+            for v in href_human:
+                href_human_list.append(v.get('href').split('/')[3])
     return href_human_list
+def get_name_human(content_html):
+    names_human_list = []
 
+    for n in dict.values(content_html):
+        soup = BeautifulSoup(n, 'lxml')
+        div_human = soup.find_all('div', class_='userCard')
+        for s in div_human:
+            href_human = s.find_all('a', class_='userCard_name')
+            for l in href_human:
+                names_human_list.append(l.text)
 
+    return names_human_list
+
+def get_name_human_pro(content_html):
+    names_human_list_pro = []
+
+    for n in dict.values(content_html):
+        soup = BeautifulSoup(n, 'lxml')
+        div_human = soup.find_all('div', class_='userCard-pro')
+        for s in div_human:
+            href_human = s.find_all('a', class_='userCard_name')
+            for l in href_human:
+               names_human_list_pro.append(l.text)
+
+    return names_human_list_pro
+# def csv_write(name_human_pro,name_company_pro,get_phone_nomer_pro):
+#     dnp=[]
+#     dnk=[]
+#     dph=[]
+#
+#     for i in name_human_pro:
+#         dnp.append(str(i))
+#     for j in name_company_pro:
+#         dnk.append(str(j))
+#     for v in get_phone_nomer_pro:
+#         dph.append(str(v))
+#
+#         d={'name':dnp,
+#            'company':dnk,
+#            'phone':dph}
+
+def csv_write(name_human_pro, name_company_pro, get_phone_nomer_pro,name_human,name_company,phone_nomer):
+    with open('gorko.csv', 'w', encoding='utf-8', ) as file:
+        writer = csv.writer(file)
+        writer.writerow(['Имя-про', 'Компания-про', 'Номер-про','Имя','Компания','Номер'])
+        writer.writerows(itertools.zip_longest(name_human_pro, name_company_pro, get_phone_nomer_pro,name_human,name_company,phone_nomer,fillvalue='-'))
 
 def main():
     base_url = 'https://ufa.gorko.ru/'
-
     html=get_html(base_url)
     data=get_page_data(html)
     content_html=get_html_company(data)
+    human=href_human(content_html)
+    html_human=get_html_human(human)
+    name_company=get_name_company(content_html)
+    phone_nomer=get_phone_nomer(html_human)
+    name_human=get_name_human(content_html)
 
-    print(content_html)
-
-
-
-
-
-
-
+    human_pro = href_human_pro(content_html)
+    html_human_pro = get_html_human_pro(human_pro)
+    name_company_pro = get_name_company_pro(content_html)
+    get_phone_nomer_pro = get_phone_number_pro(html_human_pro)
+    name_human_pro = get_name_human_pro(content_html)
+    csv_write(name_human_pro,name_company_pro,get_phone_nomer_pro,name_human,name_company,phone_nomer)
 
 if __name__=='__main__':
     main()
